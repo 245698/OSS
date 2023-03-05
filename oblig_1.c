45256122d1a03941fb10e4459ea96dfb15424dbd
245698 <115623466+245698@users.noreply.github.com>
2023-03-05 11:57:46 -0800

    Update oblig_1.c

--- oblig_1.c
+++ oblig_1.c
@@ -5,80 +5,91 @@
 #include <stdbool.h>
 #include <time.h>
 
-
-
-/* P1 skal kun få reade på ene FD, mens P2 skal kun få write på ene FD, og motsatt */
 /* Creating the file descriptor
-   Read = [0], Write = [1] 
+   First pipe sends from parent
+   Second pipe sends from child
 */
-int pipe_1[2]; // Parent read || Child write
-int pipe_2[2];  // Child read  || Parent write
+//parent write, child read
+int fileDescriptor_1[2]; //Read = [0], Write = [1]
+//child write, parent read
+int fileDescriptor_2[2]; //Read = [0], Write = [1] 
 int nrOfGuesses;
 int drawnNumber;
+int guessedNumber;
+int parentAnswer;
+int childAnswer;
+int childPromt;
+pid_t processID;
+
 
 /* Function prototypes */
 int drawRandomInteger();
-bool forkSuccessful(int id);
-bool isChildProcess(int id);
-bool isParentProcess(int id);
-
 
 
 int main(int argc, char *argv[]) {
 
-    /* Forking the process
+    
+	/* Creating the pipes and checks for errors */
+	if ( pipe(fileDescriptor_1) == -1 ) { printf("Pipe Failed!\n"); return 1; }
+	if ( pipe(fileDescriptor_2) == -1 ) { printf("Pipe Failed!\n"); return 1; }
+	
+	/* Forking the process
     returns zero in the child process.
     returns positive integer in the parent process (which is the child PID).
     returns negative integer if the fork process failed.
     */
-    int id = fork();
-
-    // checks if process is a child, return value is zero.
-    if ( isChildProcess(id) ) { 
+    processID = fork();
 	
+	/* Checks if fork was successful */
+	if (processID < 0) { printf("Fork Failed!"); return 1; }
+    
+	// Child process
+    if ( processID == 0 ) { 
+		
+		close(fileDescriptor_1[1]); //close write end of first pipe
 		printf("hello from child\n");
 		drawnNumber = drawRandomInteger();
 		printf("%d\n",drawnNumber);
 		
-		}
+		//read guess from parent
+		read(fileDescriptor_1[0], &parentAnswer, sizeof(parentAnswer));
+		
+		if (parentAnswer > drawnNumber) { childPromt = -1; }
+		else if (parentAnswer < drawnNumber) { childPromt = 1; }
+		else { childPromt = 0; }
+		
+		write(fileDescriptor_2[1], &childPromt, sizeof(childPromt));
+		
+		// Wait for parent to send an answer
+        wait(NULL);
+		
+		
+	}
         
-  
-    // checks if process is a parent, return value is non-zero.
-    else if ( isParentProcess(id) ){ printf("hello from parent\n"); }
-
+    // Parent process
+    if ( processID > 0 ) { 
+	
+		close(fileDescriptor_2[1]); //close write end of second pipe
+		printf("hello from parent\n");
+		guessedNumber = 500;
+		
+		//write guessedNumber
+		write (fileDescriptor_1[1], &guessedNumber, sizeof(guessedNumber));
+		
+		// Wait for child to send an answer
+        wait(NULL);
+		
+		// Read answer from child
+		read(fileDescriptor_2[0], &childAnswer, sizeof(childAnswer));
+		
+		if (childAnswer == -1) { printf("guess was too high!\n"); guessedNumber--; }
+		else if (childAnswer == 1) { printf("guess was too low!\n"); guessedNumber++; }
+		else { printf("guess was too correct!\n"); }
+		
+		}	
 }
 
-/* Checks wether or not a fork was sucessful or not.
-   Returns 0 (false) if input parameter is a negative integer.
-   Returns 1 (true) if input parameter is a positive integer.
-*/
-bool forkSuccessful(int id) {
-
-    if (id < 0) { printf("fork failed!\n"); return 0; }
-    else { printf("fork is successful!\n"); return 1; }
-    
-}
-
-/* Checks if a process is a child.
-   Returns 1 (true) if id
-*/
-bool isChildProcess(int id) {
-
-    if (id == 0) { return 1; }
-    else { return 0; }
-
-}
-/* Checks if a process is a parent.
-   Returns 1 (true) if id 
-*/
-bool isParentProcess(int id) {
-
-    if (id >= 1) { return 1; }
-    else { return 0; }
-
-}
-
-
+/* Functions */
 int drawRandomInteger() { 
 
 	int randomnumber;
@@ -87,11 +98,3 @@ int drawRandomInteger() {
     return randomnumber;
 	
 	}
-  
-  /*
-    getpid(); //returns the process id of it's caller
-	getppid(); //returns the process id of it's callers parent
-    pipe(pipe_1); //
-    pipe(pipe_2);
-	
-	*/
